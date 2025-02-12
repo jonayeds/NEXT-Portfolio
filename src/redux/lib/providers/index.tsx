bacbfe668c93f12ade4ee77568179011719f5bfb
jonayeds <jonayeeds@gmail.com>
2025-02-12 15:16:41 +0600

    successfully implemented reduc persist

--- src/redux/lib/providers/index.tsx
+++ src/redux/lib/providers/index.tsx
@@ -1,13 +1,17 @@
 "use client"
-import { store } from "@/redux/store"
+import { persistor, store } from "@/redux/store"
 import React from "react"
 import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react";
 
 const Providers = ({children}:{children:React.ReactNode}) => {
   return (
     <Provider store={store}>
-      
+      <PersistGate loading={null} persistor={persistor}>
+
       {children}
+      </PersistGate>
+      
       </Provider>
   )
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,16 +2,33 @@ import { configureStore } from '@reduxjs/toolkit'
 import { baseApi } from './api/baseApi'
 import loginReducer from "@/redux/features/loginState.slice"
 import authReducer from '@/redux/features/user.slice'
+import {FLUSH, PAUSE, PERSIST, persistReducer, PURGE, REGISTER, REHYDRATE} from 'redux-persist';
+import storage from "redux-persist/lib/storage";
+import { persistStore } from 'redux-persist';
+const persistConfig = {
+  key: "Auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
+
 export const store = configureStore({
     reducer: {
       [baseApi.reducerPath]: baseApi.reducer,
       login:loginReducer,
-      userLogin: authReducer
+      userLogin: persistedAuthReducer
     },
 
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(baseApi.middleware),
+    middleware: (getDefaultMiddlewares) =>
+      getDefaultMiddlewares({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }).concat(baseApi.middleware),
   })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
+
+export const persistor = persistStore(store);
